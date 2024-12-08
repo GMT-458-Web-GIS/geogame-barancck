ce866cedb7dede61b5a8ba9691f7929451f54563
Baran Çiçek <126807774+barancck@users.noreply.github.com>
2024-12-08 23:27:24 +0300

    Delete src directory

--- src/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-const NavBar = () => {
-    return (
-        <div className="navbar">
-            <span id="guess">GuessThe</span><span id="country">Country</span>
-        </div>
-    )
-}
-
-export default NavBar
--- src/services/countries.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const fields = "name,capital,area,population,flags,continents"
-const baseUrl = `https://restcountries.com/v3.1/all?fields=${fields}`
-
-export const getAllCountries = async () => {
-    const response = await axios.get(baseUrl)
-    return response.data
-}
